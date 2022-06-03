a0bbf3f9f767c1ab08ad362f881bfedc25727a4e
paschal533 <okwuosahpaschal@gmail.com>
2022-06-03 21:38:06 +0100

    update commit

--- src/App.js
+++ src/App.js
@@ -32,7 +32,7 @@ const App = () => {
         mockupImg={assets.mockup}
         banner="banner02"
       />
-      <Download />
+      //<Download />
 
       <div className="px-4 py-2 justify-center items-center bg-primary flex-col text-center banner04">
         <p className={`${styles.pText} ${styles.whiteText}`}>Made with love by {" "}
